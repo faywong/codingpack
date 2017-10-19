05605dd207b803896001b5dff319b46be9dc86cb
faywong <philip584521@gmail.com>
2017-10-19 15:39:56 +0800

    update config

---
+++ nvim_config/install_plug.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp -rf .config/ ~/
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
